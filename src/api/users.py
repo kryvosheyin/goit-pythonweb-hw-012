21dc90b8b1ceb56a15fe4eb8d325f8dd83c8e250
kryvosheyin <alex.kryvosheyin@gmail.com>
2025-01-05 22:03:28 +0000

    update avatar only for Admin users

--- src/api/users.py
+++ src/api/users.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.conf.config import settings
 from src.database.db import get_db
 from src.schemas.contacts import User
-from src.services.auth import get_current_user
+from src.services.auth import get_current_user, get_current_admin_user
 from src.services.upload_file import UploadFileService
 from src.services.users import UserService
 
@@ -16,13 +16,12 @@ limiter = Limiter(key_func=get_remote_address)
 
 
 @router.get(
-    "/me", response_model=User, description="No more than 10 requests per minute"
+    "/me",
+    response_model=User,
+    description="Too many requests! Plrease try again later.",
 )
 @limiter.limit("10 per minute")
 async def me(request: Request, user: User = Depends(get_current_user)):
-    """
-    Fetch information about current user
-    """
 
     return user
 
@@ -30,12 +29,9 @@ async def me(request: Request, user: User = Depends(get_current_user)):
 @router.patch("/avatar", response_model=User, summary="Update User avatar")
 async def update_avatar_user(
     file: UploadFile = File(),
-    user: User = Depends(get_current_user),
+    user: User = Depends(get_current_admin_user),
     db: AsyncSession = Depends(get_db),
 ):
-    """
-    Update user avatar
-    """
 
     avatar_url = UploadFileService(
         settings.CLD_NAME, settings.CLD_API_KEY, settings.CLD_API_SECRET
